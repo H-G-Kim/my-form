6b5b73d220ebacb63a086f2fe8026f5c0a2ce5bf
H-G-Kim <heegenek@gmail.com>
2024-02-12 20:48:18 +1100

    prints to console- forms

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
 
 function App() {
+
+const [fname, setFname] = useState('First name');
+const [lname, setLname] = useState('Last name');
+const [age, setAge] = useState(0);
+const [gender, setGender] =useState('Male');
+
+const handleFnameChange = (e) =>{
+  setFname(e.target.value);
+}
+
+const handleLnameChange = (e) =>{
+  setLname(e.target.value);
+}
+
+const handleGenderChange = (e) =>{
+  setGender(e.target.value);
+}
+
+const handleAgeChange = (e) =>{
+  setAge(e.target.value);
+}
+const handleInputChange =(e) =>{
+  e.preventDefault();
+  console.log("First name: " + fname + ", Last  name: " + lname+ ", Gender: " + gender+", Age: " + age )
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form onSubmit={handleInputChange}>
+        <label>
+          First Name:
+          <input type="text" value={fname} name="firstname" onChange ={handleFnameChange} />
+        </label>
+        <label>
+          Last Name:
+          <input type="text" value={lname} name="lastname" onChange ={handleLnameChange}/>
+        </label>
+        <label>
+          Age:
+          <input type="number" value={age} name="age" onChange ={handleAgeChange}/>
+        </label>
+        <label>
+          Gender:
+          <select  onChange ={handleGenderChange} value={gender}>
+            {gender}
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit"  />
+        <input type="reset" value="reset"  />
+      </form>
     </div>
   );
 }
